7c6df68642d3040b48fa278ac3ad489b26a21c58
leeyeh <lee55962698@gmail.com>
2017-03-03 17:10:26 +0800

    feat: live reload

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ AV.init({
   appId: 'ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333',
   appKey: 'ni0kwg7h8hwtz6a7dw9ipr7ayk989zo5y8t0sn5gjiel6uav',
 })
+var realtime = new AV.Realtime({
+  appId: 'ozewwcwsyq92g2hommuxqrqzg6847wgl8dtrac6suxzko333',
+})
+var LiveReload = {
+  client: null,
+  logout: function() {
+    if (this.client) {
+      this.client.close()
+      this.client = null;
+    }
+  },
+  login: function(clientId) {
+    if (this.client && this.client.id === clientId) return Promise.resolve(this.client)
+    return realtime.createIMClient(clientId).then(function(client) {
+      this.client = client
+      return client
+    }.bind(this))
+  }
+}
 
 var Todo = AV.Object.extend('Todo')
 
@@ -50,15 +69,16 @@ var app = new Vue({
     'user.objectId': {
       handler: function (id) {
         if (id) {
-          new AV.Query(Todo)
-            .equalTo('user', AV.Object.createWithoutData('User', id))
-            .find()
-            .then(function(todos) {
-              this.todos = todos.map(function(todo) {
-                return todo.toJSON()
-              })
+          this.fetchTodos(id)
+          LiveReload.login(id).then(function(client) {
+            return client.getConversation('58b8de7444d904006bee4ded')
+          }).then(function(conversation) {
+            conversation.on('message', function() {
+              if (Date.now() < this.lastReloadTime + 1000) return;
+              this.fetchTodos(id)
+              this.lastReloadTime = Date.now();
             }.bind(this))
-            .catch(alert)
+          }.bind(this))
         } else {
           this.todos = []
         }
@@ -82,7 +102,7 @@ var app = new Vue({
       set: function (done) {
         AV.Object.saveAll(
           filters[done ? 'active' : 'completed'](this.todos).map(function(todo) {
-            todo.done = done;
+            todo.done = done
             return AV.Object.createWithoutData('Todo', todo.objectId).set('done', done)
           })
         )
@@ -99,6 +119,19 @@ var app = new Vue({
   // methods that implement data logic.
   // note there's no DOM manipulation here at all.
   methods: {
+    fetchTodos: function(id) {
+      return new AV.Query(Todo)
+        .equalTo('user', AV.Object.createWithoutData('User', id))
+        .descending('createdAt')
+        .find()
+        .then(function(todos) {
+          this.todos = todos.map(function(todo) {
+            return todo.toJSON()
+          })
+        }.bind(this))
+        .catch(alert)
+    },
+
     login: function() {
       AV.User.logIn(this.username, this.password).then(function(user) {
         this.user = user.toJSON()
@@ -116,6 +149,7 @@ var app = new Vue({
     logout: function() {
       AV.User.logOut()
       this.user = null
+      LiveReload.logout()
     },
     
     addTodo: function () {
